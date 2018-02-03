9f8cfce0afe1e2040bc6a10adb4c5f155d7273ab
HassakuTb <hassaku.sc@gmail.com>
2018-02-04 00:20:21 +0900

    feat: detect tweet detail page url

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,18 @@
 //  show icon only in http://twitter,com/*/status/*
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
-  var match = /https:\/\/twitter.com\/*\/status\/*/.test(tab.url);
-  console.log(match);
-  if(match){
-    chrome.pageAction.show(tabId);
-  }
+    console.log(changeInfo);
+    if(changeInfo.status === "loading"){
+        if(createTwitterUrlRegexPattern().test(tab.url)){
+            chrome.pageAction.show(tabId);
+        }
+        else{
+            chrome.pageAction.hide(tabId);
+        }
+    }
 });
+
+
+//  create tweet detail url pattern
+function createTwitterUrlRegexPattern(){
+    return /https:\/\/twitter.com\/\w*\/status\/\d*/;
+}
