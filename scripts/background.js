a9d46a252238cc38bb9de5204d70925a001661ca
HassakuTb <hassaku.sc@gmail.com>
2018-02-09 15:50:52 +0900

    feat: add context menu

--- scripts/background.js
+++ scripts/background.js
@@ -1,18 +1,40 @@
-//  show icon only in http://twitter,com/*/status/*
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
-    console.log(changeInfo);
-    if(changeInfo.status === "loading"){
-        if(createTwitterUrlRegexPattern().test(tab.url)){
-            chrome.pageAction.show(tabId);
-        }
-        else{
-            chrome.pageAction.hide(tabId);
-        }
-    }
+function downloadOriginalImage(info, tab){
+    console.log(info);
+    //  do something
+}
+
+chrome.runtime.onInstalled.addListener(function(){
+    chrome.contextMenus.create({
+        type: 'normal',
+        id: 'downloadTwitterImage',
+        title: 'download original image',
+        contexts: [
+            'image'
+        ],
+        documentUrlPatterns: [
+            '*://twitter.com/*'
+        ]
+    });
+
+    chrome.contextMenus.onClicked.addListener(downloadOriginalImage);
 });
 
 
-//  create tweet detail url pattern
-function createTwitterUrlRegexPattern(){
-    return /https:\/\/twitter.com\/\w*\/status\/\d*/;
-}
+// //  show icon only in http://twitter,com/*/status/*
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
+//     console.log(changeInfo);
+//     if(changeInfo.status === 'loading'){
+//         if(createTwitterUrlRegexPattern().test(tab.url)){
+//             chrome.pageAction.show(tabId);
+//         }
+//         else{
+//             chrome.pageAction.hide(tabId);
+//         }
+//     }
+// });
+//
+//
+// //  create tweet detail url pattern
+// function createTwitterUrlRegexPattern(){
+//     return /https:\/\/twitter.com\/\w*\/status\/\d*/;
+// }
